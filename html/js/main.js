9a021bb7dacdf9451e85be0ab65468389e7fc7b0
wredhyn <andre@mediafresh.net>
2014-12-17 01:44:34 -0500

    First prototype

---
+++ html/js/main.js
@@ -0,0 +1,40 @@
+
+var clockSpan;
+var video;
+
+onload = function () {
+
+	// how long since start of the hour?
+	var date = new Date();
+	var dateHour = new Date(date.getFullYear(), date.getMonth(), date.getDate(), date.getHours());
+	var msSinceHour = date - dateHour;
+
+	video = document.getElementById("video");
+
+	video.currentTime = (msSinceHour + 80) / 1000;
+	video.play();
+
+	clockSpan = document.getElementById("time");
+	updateClock();
+
+}
+
+
+function updateClock() {
+
+	var date = new Date();
+	clockSpan.innerHTML = date.toLocaleTimeString();
+
+	requestAnimFrame(updateClock);
+
+}
+
+
+window.requestAnimFrame = (function(callback) {
+	return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame ||
+        function(callback) {
+          window.setTimeout(callback, 1000 / 60);
+        };
+})();
+
+
