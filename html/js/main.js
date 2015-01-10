334af19074e17ec0433f2c9328c0a1a48707965d
wredhyn <andre@mediafresh.net>
2015-01-10 15:54:36 -0500

    added styles and moved videosync to function

--- html/js/main.js
+++ html/js/main.js
@@ -1,32 +1,32 @@
-
 var clockSpan;
 var video;
 
 onload = function () {
-
-	// how long since start of the hour?
-	var date = new Date();
-	var dateHour = new Date(date.getFullYear(), date.getMonth(), date.getDate(), date.getHours());
-	var msSinceHour = date - dateHour;
-
 	video = document.getElementById("video");
-
-	video.currentTime = (msSinceHour) / 1000;
-	video.play();
-
 	clockSpan = document.getElementById("time");
+
+	// Start clock
 	updateClock();
 
+	syncVideo();
 }
 
 
 function updateClock() {
-
 	var date = new Date();
 	clockSpan.innerHTML = date.toLocaleTimeString();
 
 	requestAnimFrame(updateClock);
+}
 
+function syncVideo() {
+        // how long since start of the hour?
+        var date = new Date();
+        var dateHour = new Date(date.getFullYear(), date.getMonth(), date.getDate(), date.getHours());
+        var msSinceHour = date - dateHour;
+
+        video.currentTime = (msSinceHour + 100) / 1000;
+        video.play();
 }
 
 
