ab5b8351c75e83e644c8eaac6ecd38a2d36989d2
wredhyn <andre@mediafresh.net>
2015-01-21 22:48:26 -0500

    Added frames to clock for more precise sync checks

--- html/js/clock.js
+++ html/js/clock.js
@@ -1,9 +1,18 @@
 var Clock = function (domID) {
     var dom = document.getElementById("time");
 
+    function pad2(num) {
+    	var str = num.toString();
+    	while (str.length < 2) {
+    		str = "0"+ str;
+    	}
+    	return str;
+    }
+
     function updateClock() {
         var date = new Date();
-        dom.innerHTML = date.toLocaleTimeString();
+//         dom.innerHTML = date.toLocaleTimeString();
+        dom.innerHTML = "00:"+ pad2(date.getMinutes()) +":"+ pad2(date.getSeconds()) +":"+ pad2(Math.floor((date.getMilliseconds() / 1000) * 30));
 		requestAnimFrame(updateClock);
     }
 
