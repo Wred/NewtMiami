ce685e0631a957c387201f02cc3f8c45424878cc
wredhyn <andre@mediafresh.net>
2015-07-09 17:13:41 -0400

    tweaked acceptable thresholds

--- html/js/video.js
+++ html/js/video.js
@@ -1,12 +1,12 @@
 var Video = function (domID, startTime) {
     var dom = document.getElementById(domID),
         SEEK_TIME = 0.07, // seek takes about 70ms on my system
-        ACCEPTABLE_THRESHOLD = 1; // can be up to 1 second off before we play
+        ACCEPTABLE_THRESHOLD = .05; // can be off plus or minus this value before we play
 
     if (isNaN(dom.duration)) {
         // we haven't received the metadata yet
         dom.addEventListener('loadedmetadata', function () {
-            console.log("Got metadata.  Duration: "+ dom.duration);
+            console.log("Got metadata.  Duration: "+ dom.duration +" seconds.");
             // we have duration... we can start seek/play
             syncVideo();
         });
@@ -14,7 +14,8 @@ var Video = function (domID, startTime) {
         syncVideo();
     }
 
-    function getSecondsFromLoopStart() {
+
+    function getSecondsFromStart() {
         var time = startTime.split(":");
         // how long since start of the hour?
         var date = new Date();
@@ -24,21 +25,26 @@ var Video = function (domID, startTime) {
             // we're before the start time - add a day
             timeNow += (24 * 60 * 60 * 1000);
         }
-        var secondsFromStart = (timeNow - timeStart) / 1000;
-        return secondsFromStart % dom.duration;
+        return (timeNow - timeStart) / 1000;
+    }
+
+    function getSecondsFromLoopStart() {
+        return getSecondsFromStart() % dom.duration;
     }
 
     function syncVideo() {
         dom.removeEventListener("seeked", syncVideo);
         var currentTime = getSecondsFromLoopStart();
-        console.log("Seek completed: "+ dom.currentTime +" Current Time: "+ currentTime);
+        console.log("Seek completed at "+ dom.currentTime +"s. Current Time is "+ currentTime +"s.");
 
         // are we within an acceptable range?
-        if ((dom.currentTime >= currentTime) && (dom.currentTime < currentTime + ACCEPTABLE_THRESHOLD)) {
-            console.log("We're good to go.  Start play at: "+ dom.currentTime);
+        if ((dom.currentTime >= currentTime - ACCEPTABLE_THRESHOLD) && (dom.currentTime < currentTime + ACCEPTABLE_THRESHOLD)) {
+            console.log("We're good to go.  Start play at "+ dom.currentTime +"s. We're about "+ Math.round((currentTime - dom.currentTime) * 1000) +" milliseconds off.");
             dom.play();
+            // let's make sure we resync at the daily start time if this plays through
+            window.setTimeout((24 * 60 * 60) - getSecondsFromStart(), syncVideo);
         } else {
-            console.log("Not close enough.  Let's seek again.");
+            console.log("Not close enough - seek again.");
             dom.currentTime = currentTime + SEEK_TIME;
             dom.addEventListener("seeked", syncVideo);
         }
