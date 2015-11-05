0e397ef8f5c4cab44ddee49a9e294fd18778aaa6
wredhyn <andre@mediafresh.net>
2015-11-05 18:53:06 -0500

    Dynamically adjust play offset on a failed sync check

--- html/js/video.js
+++ html/js/video.js
@@ -3,7 +3,7 @@ var Video = function (domID, startTime, loopLength) {
         SEEK_AHEAD_TIME = 1, // give it enough time to finish seek
         playOffset = .08;  // time it takes to start play in seconds
         playTimeout = null,
-        startTimeTimeout = null,
+        loopTimeout = null,
         isFirstPlay = true,
         syncCheckInterval = null,
         ACCEPTABLE_OFFSET = .05; // in seconds
@@ -20,8 +20,6 @@ var Video = function (domID, startTime, loopLength) {
             syncVideo();
         }
     }
-
-    syncCheckInterval = setInterval(syncCheck, 2000);
 
     function getSecondsFromStart() {
         var time = startTime.split(":");
@@ -47,71 +45,107 @@ var Video = function (domID, startTime, loopLength) {
         // let's pause first.
         dom.pause();
 
+        // stop checking sync
+        clearSyncCheck();
+
         // where are we supposed to be?
         var currentTime = getSecondsFromLoopStart();
         console.log("Seeked at "+ dom.currentTime +"s. Current Time is "+ currentTime +"s.");
 
         // Do we still have time to start play?
         if ((dom.currentTime > currentTime) && (dom.currentTime < currentTime + SEEK_AHEAD_TIME)) {
-            var offset = Math.round((dom.currentTime - currentTime) * 1000) - (playOffset * 1000);
-            if (playTimeout) {
-                window.clearTimeout(playTimeout);
-            }
-            console.log("Set timeout to start play: "+ offset);
-            playTimeout = window.setTimeout(startPlay, offset);
+            // we're good.  Let's wait for the right time and play
+            setPlayTimeout(Math.round((dom.currentTime - currentTime) * 1000) - (playOffset * 1000));
         } else {
+            // seek was too slow (latency/bandwidth?)... seek again a few seconds ahead
             console.log("Seek ahead...");
             dom.addEventListener("seeked", syncVideo);
             dom.currentTime = currentTime + SEEK_AHEAD_TIME;
         }
     }
 
-    function startPlay() {
+    function setPlayTimeout(time) {
+        clearPlayTimeout();
+        console.log("Set timeout to start play: "+ time);
+        playTimeout = window.setTimeout(startPlay, time);
+    }
+
+    function clearPlayTimeout() {
         if (playTimeout) {
             window.clearTimeout(playTimeout);
             playTimeout = null;
         }
+    }
+
+    function startPlay() {
+        clearPlayTimeout();
 
         dom.play();
 
         var currentTime = getSecondsFromLoopStart();
         console.log("Started play about "+ (Math.round((currentTime - dom.currentTime) * 1000) + (playOffset * 1000)) +" milliseconds off.");
 
-        if (!startTimeTimeout) {
-            var loopTime = (loopLength - currentTime) * 1000;
-            if (loopTime > 0) {
-                // let's make sure we resync once we're past the end of the loop
-                startTimeTimeout = window.setTimeout(restartTimeout, loopTime);
-            }
+        setLoopTimeout(currentTime);
+        startSyncCheck();
+    }
+
+
+
+    function setLoopTimeout(currentTime) {
+        // set a timeout for the end of the loop (because the video file is likely longer than the actual loop because of aac/mp3 audio padding)
+        if (loopTimeout) {
+            window.clearTimeout(loopTimeout);
+            loopTimeout = null;
+        }
+
+        var loopTime = (loopLength - currentTime) * 1000;
+
+        if (loopTime > 0) {
+            // let's make sure we resync once we're past the end of the loop
+            loopTimeout = window.setTimeout(loopTimeout, loopTime);
         }
     }
 
-    function restartTimeout() {
+    function loopTimeout() {
         console.log("Past the loop - resyncing...");
         // this happens once a day to make sure we start again at the right time
-        startTimeTimeout = null;
+        loopTimeout = null;
         syncVideo();
     }
 
-    function isTimeBuffered(time) {
-        var i, l = dom.buffered.length;
-        for (i=0;i<l;i++) {
-            if ((time > dom.buffered.start(i)) && (time <= dom.buffered.end(i))) {
-                return true;
-            }
-        }
-        return false;
+
+
+    function startSyncCheck() {
+        clearSyncCheck();
+        syncCheckInterval = window.setInterval(syncCheck, 2000);
     }
 
+    function clearSyncCheck() {
+        if (syncCheckInterval) {
+            window.clearInterval(syncCheckInterval);
+            syncCheckInterval = null;
+        }
+    }
+
+    // called every few seconds to see if we're in sync
     function syncCheck() {
         var offset = getSecondsFromLoopStart() - dom.currentTime;
 
         console.log("Sync check: "+ Math.round(offset * 1000));
 
-        // if ((Math.abs(offset) > ACCEPTABLE_OFFSET) && !playTimeout) {
-        //     console.log("Resyncing...");
-        //     syncVideo();
-        // }
+        if ((Math.abs(offset) > ACCEPTABLE_OFFSET) && !playTimeout) {
+            // adjust play offset
+            playOffset += offset;
+            // to be safe, let's control this...
+            if (playOffset < 0) {
+                playOffset = 0;
+            } else if (playOffset > .2) {
+                playOffset = .2;
+            }
+
+            console.log("Adjusted play offset to: "+ playOffset +" - Resyncing...");
+            syncVideo();
+        }
     }
 
     return {
